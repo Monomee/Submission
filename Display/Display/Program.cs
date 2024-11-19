dcff6958322383efd78fdb43d03cd993c7f87011
Monomee <hoangthth12@gmail.com>
2024-11-19 21:30:41 +0700

    display

---
+++ Display/Display/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            float f = 20.5f;
+            double d = 20.5;
+            Boolean b = true;
+            char c = 'a';
+            String s = "CodeGym";
+
+            Console.WriteLine("Value of i is " + i);
+            Console.WriteLine("Value of f is " + f);
+            Console.WriteLine("Value of d is " + d);
+            Console.WriteLine("Value of b is " + b);
+            Console.WriteLine("Value of c is " + c);
+            Console.WriteLine("Value of s is " + s);
+            Console.ReadLine();
+        }
+    }
+}
