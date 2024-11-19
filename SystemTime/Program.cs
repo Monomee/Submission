a99ace15a7aeb5e7b61b3a9956ee77959d10b1e2
Monomee <hoangthth12@gmail.com>
2024-11-19 21:12:30 +0700

    main

---
+++ SystemTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine("Date time now is: " + localDate);
+            Console.ReadLine();
+        }
+    }
+}
