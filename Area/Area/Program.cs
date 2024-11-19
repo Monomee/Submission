08d51135d399aec152898fd5667f3f32857a925a
Monomee <hoangthth12@gmail.com>
2024-11-19 21:25:02 +0700

    area

---
+++ Area/Area/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter width: ");
+            float width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Area is: "+ (width*height));
+            Console.ReadLine();
+        }
+    }
+}
